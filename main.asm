2fe2b870c975e12c1b3f1da052f774012db7ec01
bob sagat <wiiqwertyuiop@gmail.com>
2024-04-22 02:36:16 -0400

    fixes

--- main.asm
+++ main.asm
@@ -1,38 +1,30 @@
 ; https://www.cs.uaf.edu/2017/fall/cs301/reference/x86_64.html
 ; https://learn.microsoft.com/en-us/cpp/build/x64-calling-convention?view=msvc-170
 
-global _main
+global main
 extern GetStdHandle, WriteConsoleA, ExitProcess
 
 section .text
 
 STD_OUTPUT_HANDLE: EQU -11
 
-_main:
-
-    ; Align the stack on a 16-byte boundary
-    and     rsp, -16
-    
-    push    rbp
-    mov     rbp, rsp
+main:
+    and rsp, -16    ; Align the stack on a 16-byte boundary
+    sub rsp, 40     ; Allocate space for parameters
 
     mov rcx, STD_OUTPUT_HANDLE
     call GetStdHandle
 
-    mov rcx, rax    ; hConsoleOutput
-    mov rdx, msg    ; *lpBuffer
-    mov r8, len     ; nNumberOfCharsToWrite
-    mov r9, 0       ; lpNumberOfCharsWritten
     push 0          ; lpReserved
+    mov r9, 0       ; lpNumberOfCharsWritten
+    mov r8, len     ; nNumberOfCharsToWrite
+    mov rdx, msg    ; *lpBuffer
+    mov rcx, rax    ; hConsoleOutput
     call WriteConsoleA
 
-    mov     rsp, rbp
-    pop     rbp
-    
+    add rsp, 40     ; Clean up stack
     mov rcx, 0
     call ExitProcess
-
-section .data
 
 msg:
     db "Hello World!", 0x0A
